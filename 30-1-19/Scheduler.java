2262d1c2e5a4ca01428e05e098a329c9be1d4968
nikhil-kharbanda <43783225+nikhil-kharbanda@users.noreply.github.com>
2019-01-30 17:25:54 -0500

    Update Scheduler.java

--- 30-1-19/Scheduler.java
+++ 30-1-19/Scheduler.java
@@ -3,14 +3,20 @@
 //L4G2 milestone 1 27/1/2019
 
 import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 import java.net.*;
 
 public class Scheduler {
 	
 	private ElevatorQueue eQueue1;
-	public ElevatorQueue.Direction CurrentDirection;
+	private ElevatorQueue.Direction CurrentDirection = ElevatorQueue.getDirection();
+	private static Direction direction = Direction.IDLE;
 	DatagramPacket sendPacket, receivePacket;
 	DatagramSocket sendSocket, receiveSocket;
+	public static final int MAX_NUM_OF_FLOORS = 10;
+	private int floor = 1;
 	
 	public Scheduler()
 	   {
@@ -54,6 +60,7 @@ public class Scheduler {
 	         e.printStackTrace();
 	         System.exit(1);
 	      }
+	      
 	      if(data[6] == (byte)1) {
 	      	eQueue1.setDirection(ElevatorQueue.Direction.UP);
 	      }
@@ -70,7 +77,82 @@ public class Scheduler {
 	      
 	      // TODO schedule packet
 	      
-	      
+	public enum Direction{ //everything that the elevator should do
+		UP, DOWN, IDLE, INACTIVE;
+	}
+	
+	
+	
+		
+	private PriorityQueue<Integer> requests = new PriorityQueue<Integer>(MAX_NUM_OF_FLOORS, new Comparator<Integer>() {
+		public int compare(Integer a, Integer b) {
+			if(direction == direction.DOWN) {
+				if(a<b) {
+					return 1;
+				}
+				if(a>b) { 
+					return -1;
+				}
+			} else if(direction == direction.UP){
+				if(a<b) {
+					return -1;
+				}
+				if(a>b) { 
+					return 1;
+				}
+			}
+			return 0;
+		}
+	});
+	
+	public void removerequests(int floor) {
+		if(requests.contains(floor)) {
+			requests.remove();
+		}
+	}
+	
+	public void addrequests(int floor) {
+		if(!requests.contains(floor)) {
+			if(requests.isEmpty()) {
+				if(this.floor < floor) {
+					setDirection(direction.UP);
+				}
+			}
+			requests.add(floor);
+		}
+	}
+	
+	public void getPassengers() {
+	}
+	
+	//GETTERS
+	
+	public boolean isIdle() {
+		return direction == direction.IDLE;
+	}
+	
+	public int getFloor() {
+		return floor;
+	}
+	
+	public PriorityQueue<Integer> getrequestss(){
+		return requests;
+	}
+	
+	public static Direction getDirection() {
+		return direction;
+	}
+	
+	//SETTERS
+	
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+	
+	public String toString() {
+		return "Direction: " + direction + "\nFloor: " + floor + "\nrequestsed Floors: " + requests;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
